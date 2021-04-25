22e1e2078b301c68ace75db2ee06277d18e41cad
Moubien Kayali <kayali@alamiah.lan>
2021-04-25 11:16:35 +0300

    initial-comit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace oop
+{
+      public class Student
+    {
+        private int _no { get; set; }
+        public int No
+        {
+            get => _no;
+            set => _no = value;
+        }
+        private int _id { get; set; }
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+        private string _name { get; set; }
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+        private float _grade { get; set; }
+        public float Grade
+        {
+            get => _grade;
+            set => _grade = value;
+        }
+        public Student() { }
+        public Student(int no, int id, string name, float grade)
+        {
+            _no = no;
+            _id = id;
+            _name = name;
+            _grade = grade;
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student(1,11,"ali",24);
+            s1.Name = "Husein";
+            
+            Console.WriteLine(s1.No+" "+s1.Name);
+            String s = "name1";
+            String s2 = "name2";
+            String r = $"{s}-{s2}";
+            Console.WriteLine(r);
+        }
+    }
+  
+
+}
